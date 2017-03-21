24dcc7db0d50edad732bbfb27c20aef48c1736a0
J Silverstein <jess.pebble@gmail.com>
2017-03-20 23:47:28 -0400

    about 2/3 done

--- lecture-notes/001-web-type-101/demo/main.js
+++ lecture-notes/001-web-type-101/demo/main.js
@@ -1,2 +1,42 @@
+console.log('hello');
 
-// tabswitcher
+(() => {
+  const tabs = (element, navClass, contClass) => {
+    const theElement = document.getElementById(element);
+    const tabNav = Array.from(theElement.getElementsByClassName(navClass));
+    const tabCont = Array.from(theElement.getElementsByClassName(contClass));
+    let activeIndex = 0;
+    let initCalled = false;
+    
+    const init = () => {
+      if (!initCalled) {
+        initCalled = true;
+        tabNav.forEach((nav) => {
+          nav.addEventListener('click', (e) => {
+            e.preventDefault();
+            goToTab(tabNav.indexOf(nav));
+          });
+        });
+      }
+    };
+    
+    const goToTab = (index) => {
+      if (index !== activeIndex && index >= 0 && index <= tabNav.length) {
+        tabNav[activeIndex].classList.remove('actv');
+        tabNav[index].classList.add('actv');
+        tabCont[activeIndex].classList.remove('actv');
+        tabCont[index].classList.add('actv');
+        activeIndex = index;
+      }
+    };
+    
+    return {
+      init,
+      goToTab
+    };
+  };
+  window.tabs = tabs;
+})();
+
+const popexTabs = tabs('popex', 'popextab', 'popexcont');
+popexTabs.init();
